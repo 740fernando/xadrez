41ea06099c1291b2a95b237e2ad987ae2ec0597d
Fernando <fernando.luiz92@hotmail.com>
2021-08-04 19:19:40 -0300

    Criando a classe Posicao

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using tabuleiro;
 
 namespace xadrez_console
 {
@@ -6,7 +7,15 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            // O sistema será dividio em 3 camadas:
+            // Camada Tabuleiro( representação de um tabuleiro e as operações basicas),
+            // Camada Jogo de Xadrez(Implementada a inteligencia do jogo de xadrez)
+            // Camada de Aplicação(É uma aplicativo no modo console que vai consumir as outras duas camadas, interagir com o usuário e executar o jogo.
+            Posicao P;
+            P = new Posicao(3, 4);
+            Console.WriteLine("Posição : "+P);
+            Console.ReadLine();
         }
     }
 }
