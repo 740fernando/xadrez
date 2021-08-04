a77090d6662548ea8730cc08d66e8710abd5bb73
Fernando <fernando.luiz92@hotmail.com>
2021-08-04 20:43:58 -0300

    Tratando erros com excecao personalizada

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -14,19 +14,22 @@ namespace xadrez_console
             // Camada Jogo de Xadrez(Implementada a inteligencia do jogo de xadrez)
             // Camada de Aplicação(É uma aplicativo no modo console que vai consumir as outras duas camadas, interagir com o usuário e executar o jogo.
 
-            Tabuleiro tab = new Tabuleiro(8, 8);
-           
-            
-            tab.colocarPeca(new Torre(tab,Cor.Preta), new Posicao(0, 0));
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
-
-            Tela.imprimirTabuleiro(tab);
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
 
-            Posicao P;
-            P = new Posicao(3, 4);
-            Console.WriteLine("Posição : "+P);
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 9));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+
+                Tela.imprimirTabuleiro(tab);
+               
+            }
+            catch(TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
             Console.ReadLine();
         }
     }
